00f2cca9858fcbf19412ef8b2e7f990901eccc19
Banana-chill-a046 <vuhoangannam04@gmail.com>
2026-05-29 21:58:54 +0700

    Delete .gitignore directory

--- .gitignore/groq-config.js
+++
@@ -1,5 +0,0 @@
-// GROQ API CONFIG - ĐẶT CÙNG THƯ MỤC VỚI index.html
-window.GROQ_CONFIG = {
-    API_KEY: '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',  // 👈 Thay bằng key thật
-    MODEL: 'mixtral-8x7b-32768'
-};
