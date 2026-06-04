807c78c8cc00af0a32bcb33c2ca31a306dbbf76a
Banana-chill-a046 <vuhoangannam04@gmail.com>
2026-06-04 15:26:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.GROQ_CONFIG = {
-    API_KEY: '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',
+    API_KEY: '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',
     MODEL: 'llama-3.1-8b-instant'
 };
